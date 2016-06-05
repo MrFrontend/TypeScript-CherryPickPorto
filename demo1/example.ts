bae6443133ce0d76ea2f5008dfbb62ab95ca5fce
Antoni Huguet Vives <antoni.huguet.vives@gmail.com>
2016-06-05 13:17:40 +0200

    Step 3, slide 38:
    Created interface for IPerson.
    Used the interface in our sortByName function.
    Used the interface to create some data of the type IPerson.
    
    Link to slide:
    http://slides.com/moix/cherrypickportotypescript#/38

--- demo1/example.ts
+++ demo1/example.ts
@@ -1,8 +1,24 @@
-function sortByName(a: any[]): any[] {
+interface IPerson {
+  name: string;
+  age: number;
+  nationality?: string | string[]; 
+}
+
+var friends: IPerson[] = [
+  { name: 'Margherita', age: 30 },
+  { name: 'Angela', age: 23 },
+  { name: 'Prashant', age: 27 },
+  { name: 'David', age: 26 }
+];
+
+function sortByName(a: IPerson[]): IPerson[] {
   var result = a.slice(0);
   result.sort(function (x, y) {
     return x.name.localCompare(y.name);
   });
   return result;
 }
-sortByName([]);
+
+var sorted = sortByName(friends);
+
+console.log(sorted);
